29f055145e43c37faaf5d1da8a5c29f3dfed5e58
Dalia <daliashanshal@gmail.com>
2019-04-01 14:13:33 -0400

    Added Dalia folder'

---
+++ Dalia/3_R.Manipulation.R
@@ -0,0 +1,12 @@
+# Setting directory
+setwd("/Users/daliashanshal/DS8008-project/Dalia")
+getwd()
+
+# Reading professions embedding
+professions_emb <- read.csv("professions_embedding_df.csv")
+View(professions_emb)
+
+#((6/7)^2)+((12/7)^2)+((18/7)^2)
+# Normalize vectors first
+#Calculate vector projection
+# Take mangnitude of it
